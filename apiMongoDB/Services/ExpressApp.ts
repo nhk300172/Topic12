6454cb26b6ae9b0faff7c51d77106133276da095
nhk300172 <khoi271003@gmail.com>
2025-02-18 21:40:09 +0700

    CRUD project

--- apiMongoDB/Services/ExpressApp.ts
+++ apiMongoDB/Services/ExpressApp.ts
@@ -1,8 +1,13 @@
 import express, { Application } from 'express';
 import path from 'path';
+import { CategoryRoute } from '../Routes/CategoryRoute';
 
 export default async (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }))
+    app.use('/assets', express.static('assets'))
+    app.use('/category', CategoryRoute)
    // app.use(express.static(path.join(__dirname, 'public')));
+
+    return app;
 }
